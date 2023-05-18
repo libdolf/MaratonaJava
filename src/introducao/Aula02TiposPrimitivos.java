6b2b7a101b1a51b2cbb4cbe55fef3c8e29083301
Lindolfo Moizinho <lindolfomoizinho1@gmail.com>
2023-05-18 11:25:21 -0300

    Update Aula02TiposPrimitivos.java
    1

--- src/introducao/Aula02TiposPrimitivos.java
+++ src/introducao/Aula02TiposPrimitivos.java
@@ -2,6 +2,15 @@ package introducao;
 
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
-        int
+        int inteiro = 1;
+        long numeroGrande = 100000000;
+        double numeroRacional = 1.5;
+        float racionalMaior = 60023.5f;
+        byte numeroByte = 10;
+        short inteiroPequeno = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'a';
+
     }
 }
