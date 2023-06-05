617148ac8bd433d278721482553de89bb439ffc0
lindolfo <lindolfomoizinho1@gmail.com>
2023-06-05 17:27:14 -0300

    !

--- src/introducao/Aula01OlaMundo.java
+++ src/introducao/Aula01OlaMundo.java
@@ -2,6 +2,6 @@ package introducao;
 
 public class Aula01OlaMundo {
     public static void main(String[] args) {
-        System.out.println("Olá Mundo!");
+        System.out.println("Olá Mundo!");.
     }
 }
